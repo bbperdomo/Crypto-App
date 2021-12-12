875a08f19770ae34ae94cfc1e539afea8bea5532
bperdomo2016 <bperdomo2016@fau.edu>
2021-12-12 03:49:07 -0500

    created favorites activity, Gson attempt, Intent attempt at retrieving currency arraylist from adapter activity

--- app/src/main/java/com/example/cryptoapptestc/Favorites.java
+++ app/src/main/java/com/example/cryptoapptestc/Favorites.java
@@ -1,6 +1,7 @@
 package com.example.cryptoapptestc;
 
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.annotation.NonNull;
@@ -19,8 +20,10 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -37,13 +40,56 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
+import java.io.Serializable;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class Favorites {
+import java.lang.reflect.Type;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+public class Favorites extends AppCompatActivity implements Serializable{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.currency_rv_item);
+
+        Bundle bundle = getIntent().getExtras();
+        String jsonString = bundle.getString("KEY");
+        Gson gson = new Gson();
+        Type listOfCurrencyType = new TypeToken<List<CurrencyModal>>() {}.getType();
+        ArrayList<CurrencyModal> modals = gson.fromJson(jsonString, listOfCurrencyType);
+        System.out.println(modals);
+
+        //getIncomingIntent();
+    }
+
+    private void getIncomingIntent(){
+
+
+        Intent intent = getIntent();
+        //ArrayList<CurrencyModal> mylist = (ArrayList<CurrencyModal>)intent.getSerializableExtra("mylist");
+
+        //setString(mylist);
+
+
+    }
+
+    private void setString(ArrayList mylist){
+
+        TextView name = findViewById(R.id.textView);
+        //name.setText((CharSequence) mylist);
+
+        //Button name = findViewById(R.id.button2);
+        //name.setText((CharSequence) mylist);
+    }
 
 
 }
